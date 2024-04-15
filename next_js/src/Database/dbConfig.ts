72a875ad8e1f2ed545878e8455a31dc996891d84
Subhamay-Dey <ncdey1966@gmail.com>
2024-04-15 15:31:38 +0530

    rendered the messages for the successfull connectionof out mongodb and also if failed so and also updated my .env file with the domain url, token secret for confirmation of the user.

--- next_js/src/Database/dbConfig.ts
+++ next_js/src/Database/dbConfig.ts
@@ -3,8 +3,18 @@ import mongoose from "mongoose";
 export async function connectToDatabase(){
     try {
 
-        mongoose.connect(process.env.MONGO_URL!)
+        mongoose.connect(process.env.MONGO_URI!)
         const connection = mongoose.connection
+
+        connection.on('connected', () => {
+            console.log('MongoDB connected');
+        })
+
+        connection.on('error', (err) => {
+            console.log("MongoDB connection error, please make sure db is up and running" + err);
+            process.exitCode
+            
+        })
 
     } catch (error) {
         console.log('Something went wrong in connecting to DB');
