012e58ad94962978846310169c71d94a53246e8b
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-10 23:48:04 +0530

    mailer component sucessfully completed, use the nodemailer transporter and gave the mailResponse to the prescribed logging in user from my profile.

--- next-auth_project2/src/app/helpers/mailer.ts
+++ next-auth_project2/src/app/helpers/mailer.ts
@@ -2,8 +2,27 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async({email, emailType, userId}) => {
     try {
-        
-    } catch (error) {
-        
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 465,
+            secure: false, 
+            auth: {
+              user: "maddison53@ethereal.email",
+              pass: "jn7jnAPss4f63QBp6D",
+            },
+          });
+
+          const mailOptions = {
+            from: 'ncdey1966@gmail.com',
+            to: email,
+            subject: emailType == 'VERIFY' ? 'Verify your email' : 'rsest your password',
+            html: "<b>Hello world?</b>",
+          }
+
+          const mailResponse = await transporter.sendMail(mailOptions)
+          return mailResponse;
+
+    } catch (error:any) {
+        throw new Error(error.message)
     }
 }
