ecaf4bbc62dde37e4b8870a1fee9781ac142501c
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-10 13:46:21 +0530

    password generator by bcryptjs and successfully created new user with the given properties in newUser and saved it.

--- next-auth_project2/src/app/api/users/signup/route.ts
+++ next-auth_project2/src/app/api/users/signup/route.ts
@@ -18,8 +18,27 @@ export async function POST(request: NextRequest) {
         if(user){
             return NextResponse.json({error: "User already exists"}, {status: 400})
         }
+
+        //password
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
         
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        })
         
+        const savedUser = await newUser.save();
+        console.log(savedUser);
+        
+
+        return NextResponse.json({
+            message: "User created successfully",
+            user: savedUser,
+            sucess: true
+        })
+
     } catch (error: any) {
         return NextResponse.json({error: error.message}, 
             {status: 500}
