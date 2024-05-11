3c72ea7dd7bd6a90da0b01d9275ae6c7ac5da14e
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-11 15:31:10 +0530

    signup page ui successfully completed and also used useState in useEffect to allow to sign up only when all the input fields are filled.

--- next-auth_project2/src/app/signup/page.tsx
+++ next-auth_project2/src/app/signup/page.tsx
@@ -1,8 +1,77 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function SignupPage() {
+
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  const onSignUp = async () => {
+      try {
+        
+      } catch (error) {
+        
+      }
+  }
+
+  useEffect(() => {
+    
+    if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+    
+  }, [user])
+  
+
   return (
-    <div>SignupPage</div>
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1>Signup</h1>
+        <hr />
+        <label htmlFor="username">username</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="username"
+            type="text"
+            value={user.username}
+            onChange={(e) => setUser({...user, username: e.target.value})}
+            placeholder="username"
+            />
+        <label htmlFor="email">email</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="email"
+            type="text"
+            value={user.email}
+            onChange={(e) => setUser({...user, email: e.target.value})}
+            placeholder="email"
+            />
+        <label htmlFor="password">password</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="password"
+            type="password"
+            value={user.password}
+            onChange={(e) => setUser({...user, password: e.target.value})}
+            placeholder="password"
+            />
+            <button
+            onClick={onSignUp}
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisabled ? "No signup" : "Signup"}</button>
+            <Link href="/login">Visit login page</Link>
+        </div>
+    </>
   )
 }
 
