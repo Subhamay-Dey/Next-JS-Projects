2c4455fd75150d10f0a0f2062c667e8a055eee8f
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-11 22:51:04 +0530

    a critical bug has come which is almost fixed.

--- next-auth_project2/src/database/database.ts
+++ next-auth_project2/src/database/database.ts
@@ -1,21 +1,46 @@
 import mongoose from "mongoose";
 
-export async function connect() {
-    try {
+type ConnectionObject = {
+    isConnected?: number;
+  };
+  
+  const connection: ConnectionObject = {};
 
-        await mongoose.connect(process.env.MONGO_URL!);
-        const connection = mongoose.connection;
+export async function connect(): Promise<void>  {
+    // try {
+
+    //     await mongoose.connect(process.env.MONGO_URL!);
+    //     const connection = mongoose.connection;
         
-        connection.on('connected', () => {
-            console.log("MongoDb connected");
-        })
-        connection.on('error', (err) => {
-            console.log("MongoDb connection failed:" + err);
-            process.exit();
-        })
+    //     connection.on('connected', () => {
+    //         console.log("MongoDb connected");
+    //     })
+    //     connection.on('error', (err) => {
+    //         console.log("MongoDb connection failed:" + err);
+    //         process.exit();
+    //     })
         
-    } catch (error) {
-        console.log("Something went wrong in conecting to DataBase");
-        console.log(error);
-    }
+    // } catch (error) {
+    //     console.log("Something went wrong in conecting to DataBase");
+    //     console.log(error);
+    // }
+
+    if (connection.isConnected) {
+        console.log('Already connected to the database');
+        return;
+      }
+    
+      try {
+        // Attempt to connect to the database
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+    
+        connection.isConnected = db.connections[0].readyState;
+    
+        console.log('Database connected successfully');
+      } catch (error) {
+        console.error('Database connection failed:', error);
+    
+        // Graceful exit in case of a connection error
+        process.exit(1);
+      }
 }
