a1bf6c915be9a51765b209664da8df9fa6d81eea
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-12 05:31:42 +0530

    updated.

--- next-auth_project2/src/app/api/users/signup/route.ts
+++ next-auth_project2/src/app/api/users/signup/route.ts
@@ -4,7 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import { sendEmail } from "@/helpers/mailer";
 
-
 export async function POST(request: NextRequest) {
     try {
 
--- next-auth_project2/src/database/database.ts
+++ next-auth_project2/src/database/database.ts
@@ -1,15 +1,9 @@
 import mongoose from "mongoose";
 
-// type ConnectionObject = {
-//     isConnected?: number;
-//   };
-  
-//   const connection: ConnectionObject = {};
-
 export async function connect(): Promise<void>  {
     try {
 
-        await mongoose.connect(process.env.MONGO_URL!);
+        await mongoose.connect(process.env.MONGO_URI!);
         const connection = mongoose.connection;
         
         connection.on('connected', () => {
@@ -24,23 +18,4 @@ export async function connect(): Promise<void>  {
         console.log("Something went wrong in conecting to DataBase");
         console.log(error);
     }
-
-//     if (connection.isConnected) {
-//         console.log('Already connected to the database');
-//         return;
-//       }
-    
-//       try {
-//         // Attempt to connect to the database
-//         const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
-    
-//         connection.isConnected = db.connections[0].readyState;
-    
-//         console.log('Database connected successfully');
-//       } catch (error) {
-//         console.error('Database connection failed:', error);
-    
-//         // Graceful exit in case of a connection error
-//         process.exit(1);
-//       }
 }
