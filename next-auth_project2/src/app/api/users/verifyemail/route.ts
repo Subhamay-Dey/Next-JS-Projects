f9f6ab7f80f54da51fed6bb98203859a21cc6607
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-13 20:07:54 +0530

    User verification is successfully completed.

---
+++ next-auth_project2/src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,42 @@
+import {connect} from "@/database/database";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
+
+connect();
+
+export async function POST(request:NextRequest){
+    try {
+        const reqBody = await request.json();
+
+        const {token} = reqBody
+        console.log(token);
+        
+        const user = await User.findOne({
+            verifyToken: token,
+            verifyTokenExpiry:{$gt: Date.now()}
+        })
+
+        if(!user) {
+            return NextResponse.json({error: "Invalid token or your token has expired."},
+            {status: 400})
+        }
+        console.log(user);
+
+        user.isVerfied = true
+        user.verifyToken = undefined
+        user.verifyTokenExpiry = undefined
+
+        await user.save();
+
+        return NextResponse.json({
+            message: "User successfully verified",
+            success: true
+        }, {status: 400})
+        
+    } catch (error: any) {
+        return NextResponse.json({error: error.message},
+        {status: 500})
+    }
+}
